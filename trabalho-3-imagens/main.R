466ef8271031bdc5f9dad6bb37460bbdf11818f7
Igor Freire <igor.auad@gmail.com>
2014-12-11 23:53:36 -0600

    Atualizacoes do Ramon no script R

--- trabalho-3-imagens/main.R
+++ trabalho-3-imagens/main.R
@@ -1,8 +1,18 @@
 # Library for multicore processing:
-library(doMC)
-registerDoMC()
+#library(doMC)
+#registerDoMC()
 
+library(foreach)
 load('matlab/data/data.Rd')
+
+# Concatenate d.train and d.test and order the resulting dataframe by row names.
+# This is done so that d.train is identical to training.mat, used by MATLAB.
+d.train <- rbind(d.train, d.test);
+d.train <- d.train[ order( as.integer(row.names(d.train)) ), ];
+
+# Do the same for im.train and im.test
+im.train <- rbind(im.train, im.test);
+im.train <- im.train[ order( as.integer(substring(row.names(im.train), 8)) ), ];
 
 # visualize image
 # Reverse because R's image function expects the origin to be in the lower left corner
@@ -41,66 +51,157 @@ predictions <- data.frame(ImageId = 1:nrow(d.test), p)
 # See "Matching by correlation" in 
 # http://www.math.hcmuns.edu.vn/~tatuana/Xu%20Ly%20Anh/Ebook(Seminar)/Digital%20Image%20Processing%20CHAPTER%2012%20%5BGONZALEZ%20R%20C%20WOOD%5D.pdf
 # Obs: some methods use FFT to compute correlation
+# 
 
 source('meanPatch.R')
 source('predictWithCorrelation.R')
 
-coord      <- "mouth_center_top_lip" 
+coord      <- "mouth_center_top_lip"
 coord_x <- paste(coord, "x", sep="_")
 coord_y <- paste(coord, "y", sep="_")
 
-patch_size <- 15 # e.g. 10 means we will have a square of 21x21 pixels (10+1+10). 
-search_size <- 10 # e.g. would give a 5x5 (2+1+2) 
+patch_size <- 15 # e.g. 10 means we will have a square of 21x21 pixels (10+1+10).
+search_size <- 10 # e.g. 2 would give a 5x5 (2+1+2)
 
-mean.patch <- meanPatch(coord, patch_size, TRUE)  
+mean.patch <- meanPatch(coord, patch_size, TRUE)
 
-# Vizualize prediciton
-iImg = 2
-estimated_p <- predictWithCorrelation(coord, search_size, mean.patch, iImg, TRUE) 
-points(96 - d.test$mouth_center_top_lip_x[iImg], 96 - d.test$mouth_center_top_lip_y[iImg], col="green")
+# Visualize prediction
+iImg = 2;
+estimated_p <- predictWithCorrelation(coord, search_size, mean.patch, iImg, TRUE)
+points(96 - d.test$mouth_center_top_lip_x[iImg], 96 - d.test$mouth_center_top_lip_y[iImg], col="green", lwd=2, cex=1.5)
 real_p <- c(d.test$mouth_center_top_lip_x[iImg], d.test$mouth_center_top_lip_y[iImg])
 err <- estimated_p - real_p
 err
 
-# Iterate over all test images and compute prediction
+
+# read matlab/data/testIdx.csv to get the indices of the test images
+testIndices <- read.csv(file="./matlab/data/testIdx.csv", header=TRUE, sep=",", colClasses=c("NULL", NA))
+
+
+# Iterate over the train images without NAN-entries (indices from 1 to 470 in targetIdx)
+# all test images and compute prediction
 # NOTE: for many the chosen feature may be missing
 # nrow(d.test)
-targetIdx <- 1:30
-est_p <- foreach(iImg = targetIdx, .combine=rbind) %dopar% {  
-  p <- predictWithCorrelation(coord, search_size, mean.patch, iImg, FALSE)    
-  p
-}
-colnames(est_p) <- c(coord_x, coord_y)
-est_p
 
-rmse <- sqrt(mean((d.test[targetIdx,c(coord_x, coord_y)] - est_p)^2, na.rm=T))
-rmse
+targetIdx <- testIndices[1:50,1] #1:470
+features <- cbind("mouth_center_top_lip", "mouth_center_bottom_lip", "mouth_left_corner", "mouth_right_corner")
 
-#
-# Search the best patch_size and search_size (based on RM)
-#
+i_RMSEs <- 0
 
-coord      <- "mouth_center_bottom_lip" 
-coord_x <- paste(coord, "x", sep="_")
-coord_y <- paste(coord, "y", sep="_")
-
-patch_sizes <- 4:25 # e.g. 10 means we will have a square of 21x21 pixels (10+1+10). 
-search_size <- 3:30 # e.g. would give a 5x5 (2+1+2) 
-
-targetIdx <- 1:200
-
-rmses <- foreach(patchSize = patch_sizes, .combine=rbind) %dopar% {
+RMSEs <- foreach(coord = features, .combine=rbind) %do% { #%dopar% {
   
-  mean.patch <- meanPatch(coord, patchSize, FALSE) 
-  est_p <- foreach(iImg = targetIdx, .combine=rbind) %do% {  
-    p <- predictWithCorrelation(coord, search_size, mean.patch, iImg, FALSE)    
+  print( paste("i_RMSEs =", i_RMSEs) );
+  
+  j_est <- 0
+  mean.patch <- meanPatch(coord, patch_size, TRUE)
+  est_p <- foreach(iImg = targetIdx, .combine=rbind) %do% { #%dopar% {  
+    p <- predictWithCorrelation(coord, search_size, mean.patch, iImg, FALSE)
+    if(j_est%%100 == 0) {
+      print( paste("j_est =", j_est) );
+    }
+    
+    j_est <- j_est + 1
+    
     p
   }
-
-  rmse <- sqrt(mean((d.test[targetIdx,c(coord_x, coord_y)] - est_p)^2, na.rm=T))
-  print(rmse)
-  rmse  
+  
+  i_RMSEs  <- i_RMSEs + 1
+  
+  coord_x <- paste(coord, "x", sep="_")
+  coord_y <- paste(coord, "y", sep="_")
+  
+  colnames(est_p) <- c(coord_x, coord_y)
+  #est_p
+  
+  rmse <- sqrt(mean((d.train[targetIdx,c(coord_x, coord_y)] - est_p)^2, na.rm=T))
+  rmse
 }
+
+RMSE_global <- mean(RMSEs)
+print(RMSE_global)
+
+
+# Show comparison of real vs detected bounding boxes for test figure #50
+#iImg = 170; #testIndices[10,1]
+iImg = testIndices[50,1]
+im  <- matrix(data=rev(im.train[iImg,]), nrow=96, ncol=96)
+#im  <- matrix(data=im.train[iImg,], nrow=96, ncol=96)
+image(1:96, 1:96, im, col=gray((0:255)/255))
+
+real_leftCorner <- cbind(96-d.train$mouth_left_corner_x[iImg], 96-d.train$mouth_left_corner_y[iImg]);
+real_rightCorner <- cbind(96-d.train$mouth_right_corner_x[iImg], 96-d.train$mouth_right_corner_y[iImg]);
+real_topLip <- cbind(96-d.train$mouth_center_top_lip_x[iImg], 96-d.train$mouth_center_top_lip_y[iImg]);
+real_bottomLip <- cbind(96-d.train$mouth_center_bottom_lip_x[iImg], 96-d.train$mouth_center_bottom_lip_y[iImg]);
+
+rect( real_leftCorner[1], real_bottomLip[2], real_rightCorner[1], real_topLip[2], lwd=2, border="red" );
+
+
+# draw detected bounding box
+features <- cbind("mouth_center_top_lip", "mouth_center_bottom_lip", "mouth_left_corner", "mouth_right_corner")
+points <- foreach(coord = features, .combine=rbind) %do% { #%dopar% {
+  
+  mean.patch <- meanPatch(coord, patch_size, FALSE)
+  p <- predictWithCorrelation(coord, search_size, mean.patch, iImg, FALSE)
+  p
+}
+
+detected_topLip = 96 - points[1,];
+detected_bottomLip = 96 - points[2,];
+detected_leftCorner = 96 - points[3,];
+detected_rightCorner = 96 - points[4,];
+
+# points(detected_topLip[1], detected_topLip[2], col="green", lwd=2, cex=1.5)
+# points(detected_bottomLip[1], detected_bottomLip[2], col="green", lwd=2, cex=1.5)
+# points(detected_leftCorner[1], detected_leftCorner[2], col="green", lwd=2, cex=1.5)
+# points(detected_rightCorner[1], detected_rightCorner[2], col="green", lwd=2, cex=1.5)
+
+rect( detected_leftCorner[1], detected_bottomLip[2], detected_rightCorner[1], detected_topLip[2], lwd=2, border="green" );
+
+png(filename="figs/correlation_comparison.png")
+plot(fit)
+dev.off()
+
+
+# coord = "mouth_left_corner"
+# mean.patch <- meanPatch(coord, patch_size, TRUE)
+# detected_leftCorner <- predictWithCorrelation(coord, search_size, mean.patch, iImg, FALSE)
+# 
+# coord = "mouth_right_corner"
+# mean.patch <- meanPatch(coord, patch_size, TRUE)
+# detected_rightCorner <- predictWithCorrelation(coord, search_size, mean.patch, iImg, FALSE)
+# 
+# coord = "mouth_center_top_lip"
+# mean.patch <- meanPatch(coord, patch_size, TRUE)
+# detected_topLip <- predictWithCorrelation(coord, search_size, mean.patch, iImg, FALSE)
+
+
+
+# #
+# # Search the best patch_size and search_size (based on RMSE)
+# #
+# 
+# coord      <- "mouth_center_bottom_lip" 
+# coord_x <- paste(coord, "x", sep="_")
+# coord_y <- paste(coord, "y", sep="_")
+# 
+# patch_sizes <- 4:25 # e.g. 10 means we will have a square of 21x21 pixels (10+1+10). 
+# search_size <- 3:30 # e.g. would give a 5x5 (2+1+2) 
+# 
+# targetIdx <- 1:200
+# 
+# rmses <- foreach(patchSize = patch_sizes, .combine=rbind) %do% { #%dopar% {
+#   
+#   mean.patch <- meanPatch(coord, patchSize, FALSE) 
+#   est_p <- foreach(iImg = targetIdx, .combine=rbind) %do% {  
+#     p <- predictWithCorrelation(coord, search_size, mean.patch, iImg, FALSE)    
+#     p
+#   }
+# 
+#   rmse <- sqrt(mean((d.test[targetIdx,c(coord_x, coord_y)] - est_p)^2, na.rm=T))
+#   print(rmse)
+#   rmse  
+# }
+
 
 
 
